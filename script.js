5f517f5b875530618b29cc66176f76fcba29acaa
Mohd Shadab <shadab_ibrahim@outlook.com>
2023-06-28 21:58:27 +0530

    submitting code-Wed Jun 28 2023 21:58:27 GMT+0530 (India Standard Time)

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let list = document.querySelectorAll("li");
+for(let i=0; i<list.length; i++)
+{
+   if(list[i].id==="level")
+   {
+    alert(`The level of the element is: ${i}`);
+    break;
+   }
+}
